34d02f47aac72a704b0266dfccfaa2d6292e9c3a
rendhive <rendhivecode@gmail.com>
2026-06-11 17:52:15 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul abc.py

---
+++ 1. Mengimpor Modul abc.py
@@ -0,0 +1,5 @@
+from abc import ABC, abstractmethod
+
+print("Modul abc berhasil diimpor.")
+# Fungsi: Mengimpor modul abc untuk mendefinisikan kelas dan metode abstrak.
+# Kondisi: Digunakan sebelum mendefinisikan kelas abstrak dan metode.

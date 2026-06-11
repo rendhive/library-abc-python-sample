21be8b82f49ba2b2bbbd15f29460e08546d9ff04
rendhive <rendhivecode@gmail.com>
2026-06-11 18:12:40 +0700

    [DOCT] Sample Library Python: 2. Membuat Kelas Abstrak.py

---
+++ 2. Membuat Kelas Abstrak.py
@@ -0,0 +1,7 @@
+from abc import ABC
+
+class Shape(ABC):
+    pass  # Kelas abstrak tidak memiliki implementasi masih
+
+# Fungsi: Mendeskripsikan kelas abstrak yang tidak dapat diinstansiasi langsung.
+# Kondisi: Ketika Anda ingin mendefinisikan kelas dasar untuk hierarki kelas lainnya.
